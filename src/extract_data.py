4a41c8bfc3c8fd235ec82ff0354bdf5844fc746f
unknown <leepoewun@gmail.com>
2026-01-27 00:23:36 +0800

    slight change to add back logger

--- src/extract_data.py
+++ src/extract_data.py
@@ -5,6 +5,7 @@ import pandas as pd
 from utils.file_utils import extract_csv_to_df, save_df_to_csv, generate_dir
 from utils.metadata_utils import parse_raw_details,enrich_raw_df_with_details, parse_staging_pathname
 
+logging.getLogger(__name__)
 
 def extract_raw_to_stage(files:Generator, dest_root_path:Path, raw_details_config:dict[str:str])->None:
     """Extract raw CSV with metadata enrichment, then save to staging directory"""
